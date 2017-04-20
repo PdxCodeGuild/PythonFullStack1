37b71c016d73b997c883e4a9022f096a7d6b6b27
K. Prasch <kieranprasch@gmail.com>
2017-04-20 09:16:02 -0700

    Daily Notebook 8 and Fibo Lab Renamed.

---
+++ 1_Python/5_Advanced_Python/labs/fibo_generator/fibo_gen.py
@@ -0,0 +1,30 @@
+"""
+Write a Generator function named 'fib_infinite()' to calculate fibbonacci numbers to infinity.
+
+>>> fibo_iterator = fib_infinite()
+
+>>> next(fibo_iterator)
+0
+
+>>> next(fibo_iterator)
+1
+
+>>> next(fibo_iterator)
+1
+
+>>> next(fibo_iterator)
+2
+
+>>> next(fibo_iterator)
+3
+
+>>> next(fibo_iterator)
+5
+
+>>> next(fibo_iterator)
+8
+
+>>> next(fibo_iterator)
+13
+
+"""
